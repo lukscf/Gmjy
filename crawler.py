a20e42c207492a38bc03153086d9730ca40f5ac5
Lucas Cruz Ferreira <lucascruzferreira@gmail.com>
2025-06-02 17:03:30 -0300

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,3 +1,5 @@
+# Lembrar de colocar as bibliotecas necessárias: selenium, panda, unicode, prompt_toolkit.
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
